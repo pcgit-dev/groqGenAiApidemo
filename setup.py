63a30b51f15a06bf70640adc197c911f8116d244
MyCommits <prakash.chandra.mailbox@gmail.com>
2025-03-02 11:03:53 +0400

    Deve branch commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'Groq API Project',
-    version= '0.0.0',
+    version= '0.0.1',
     author= 'Prakash',
     packages= find_packages(),
     install_requires = []  
